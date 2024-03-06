d4a5b548622e39b35e047b8e709a6d676e323467
mitu baby <ucihazhaske@gmail.com>
2024-03-06 15:05:43 +0700

    npm init, add express, add nodemon, add cariMobil.html, styling cariMobil.html, and add HTTP Server,

--- BinarCarRental/probable-garbanzo/server/index.js
+++ BinarCarRental/probable-garbanzo/server/index.js
@@ -1,1 +1,35 @@
-console.log("Implement servermu disini yak 😝!");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Menyajikan file statis dari direktori 'public'
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "../", "public", "images"))
+);
+
+// Konfigurasi Express untuk menyajikan file statis dari direktori 'css'
+app.use("/css", express.static(path.join(__dirname, "../", "public", "css")));
+
+// Konfigurasi Express untuk menyajikan file statis dari direktori 'scripts'
+app.use(
+  "/scripts",
+  express.static(path.join(__dirname, "../", "public", "scripts"))
+);
+
+// Rute untuk "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "public", "index.html"));
+});
+
+// Rute untuk "/cars"
+app.get("/cars", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "public", "cariMobil.html"));
+});
+
+// Mulai server
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
