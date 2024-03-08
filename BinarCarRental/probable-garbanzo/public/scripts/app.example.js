c5d553d5e238cab68eb1133caa727be6fb627824
mitu baby <ucihazhaske@gmail.com>
2024-03-08 21:36:18 +0700

    Add Feature filter by date, time, and passanger

--- BinarCarRental/probable-garbanzo/public/scripts/app.example.js
+++ BinarCarRental/probable-garbanzo/public/scripts/app.example.js
@@ -3,6 +3,7 @@ class App {
     this.clearButton = document.getElementById("clear-btn");
     this.loadButton = document.getElementById("load-btn");
     this.carContainerElement = document.getElementById("card-cars");
+    this.kirim = document.getElementById("btn-submit");
   }
 
   async init() {
@@ -10,11 +11,47 @@ class App {
 
     // Register click listener
     this.clearButton.onclick = this.clear;
-    this.loadButton.onclick = this.run;
+    // this.loadButton.onclick = this.run;
+    this.kirim.onclick = this.run;
   }
 
-  run = () => {
-    Car.list.forEach((car) => {
+  run = (event) => {
+    event.preventDefault();
+
+    // Ambil value dari input tanggal dan jumlah penumpang
+    const inputDate = document.getElementById("tanggal").value;
+    const inputTime = document.getElementById("time").value;
+    const inputPassenger = parseInt(
+      document.getElementById("jumlahPenumpang").value
+    );
+
+    function convertTimeToMilliseconds(time) {
+      // Pisahkan jam dan menit
+      const [hours, minutes] = time.split(":");
+
+      // Hitung total milidetik dari jam dan menit
+      return parseInt(hours) * 60 + parseInt(minutes);
+    }
+
+    const milidetik = convertTimeToMilliseconds(inputTime);
+
+    // Filter mobil berdasarkan ketersediaan tanggal dan jumlah penumpang
+    const filteredCars = Car.list.filter((car) => {
+      const availableAt = new Date(car.availableAt);
+      const isoDate = availableAt.toISOString().split("T")[0];
+      const time = availableAt.getHours() * 60 + availableAt.getMinutes();
+      return (
+        isoDate >= inputDate &&
+        time >= milidetik &&
+        car.capacity >= inputPassenger
+      );
+    });
+
+    // Bersihkan konten sebelum menampilkan mobil yang difilter
+    this.clear();
+
+    // Render hanya mobil yang telah difilter
+    filteredCars.forEach((car) => {
       const node = document.createElement("div");
       node.classList.add("col-md-4");
       node.innerHTML = car.render();
